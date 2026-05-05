556ed1f4e458b9015a4faafa32b7dfefd3a385b9
doctorkotik187 <217476020+doctorkotik187@users.noreply.github.com>
2026-05-05 17:48:27 +0900

    vibe coded /project/:id/update! a LOT

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,4 +1,4 @@
--- Place your queries here. Docs available https://www.hugsql.org/
+-- Place your queries here. Docs available [https://www.hugsql.org/](https://www.hugsql.org/)
 
 -- :name get-projects :? :*
 SELECT *
@@ -12,32 +12,42 @@ WHERE list = :list
 ORDER BY state, priority, updated_at DESC;
 
 -- :name get-project :? :1
-SELECT * FROM projects WHERE id = :id;
+SELECT *
+FROM projects
+WHERE id = :id;
 
 -- :name create-project! :! :n
-INSERT INTO projects(
-title, description, is_personal,
-programming_lang, has_test_suite, is_open_source,
-hourly_rate_cents
+INSERT INTO projects (
+  title,
+  description,
+  is_personal,
+  programming_lang,
+  has_test_suite,
+  is_open_source,
+  hourly_rate_cents
 ) VALUES (
-:title, :description, :is_personal,
-:programming_lang, :has_test_suite, :is_open_source,
-:hourly_rate_cents
+  :title,
+  :description,
+  :is_personal,
+  :programming_lang,
+  :has_test_suite,
+  :is_open_source,
+  :hourly_rate_cents
 );
 
 -- :name update-project! :! :n
 UPDATE projects
-SET title = :title,
-    description = :description,
-    details = :details,
-    is_personal = :is_personal,
-    programming_lang = :programming_lang,
-    has_test_suite = :has_test_suite,
-    is_open_source = :is_open_source,
-    list = :list,
-    state = :state,
-    priority = :priority,
-    hourly_rate_cents = :hourly_rate_cents,
-    minutes_worked = :minutes_worked,
+SET title = COALESCE(:title, title),
+    description = COALESCE(:description, description),
+    details = COALESCE(:details, details),
+    is_personal = COALESCE(:is_personal, is_personal),
+    programming_lang = COALESCE(:programming_lang, programming_lang),
+    has_test_suite = COALESCE(:has_test_suite, has_test_suite),
+    is_open_source = COALESCE(:is_open_source, is_open_source),
+    list = COALESCE(:list, list),
+    state = COALESCE(:state, state),
+    priority = COALESCE(:priority, priority),
+    hourly_rate_cents = COALESCE(:hourly_rate_cents, hourly_rate_cents),
+    minutes_worked = COALESCE(:minutes_worked, minutes_worked),
     updated_at = now()
 WHERE id = :id;
--- src/clj/kit/dk_magic_forge/web/controllers/project.clj
+++ src/clj/kit/dk_magic_forge/web/controllers/project.clj
@@ -6,17 +6,9 @@
    [clojure.string :as str]
    [clojure.tools.logging :as log]))
 
-;; ------------------------------------------------------------
-;; constants
-;; ------------------------------------------------------------
-
 (def valid-lists #{"inbox" "projects" "someday" "archives"})
 (def valid-states #{"doing" "todo" "waiting" "noop" "done" "canceled"})
 (def valid-priorities #{"a" "b" "c" "d"})
-
-;; ------------------------------------------------------------
-;; helpers
-;; ------------------------------------------------------------
 
 (defn blank? [s]
   (or (nil? s) (str/blank? s)))
@@ -46,12 +38,16 @@
       (= "waiting" s) "waiting"
       :else "idle")))
 
-(defn present-keys [m]
-  (into {} (remove (comp nil? val) m)))
+(defn checkbox-checked? [params k]
+  (contains? params k))
 
-;; ------------------------------------------------------------
-;; core view model
-;; ------------------------------------------------------------
+(defn render-project-list [request template projects]
+  (layout/render request template
+                 {:projects projects
+                  :valid_lists valid-lists
+                  :valid_states valid-states
+                  :valid_priorities valid-priorities
+                  :flash (:flash request)}))
 
 (defn enrich-project [p]
   (-> p
@@ -62,21 +58,33 @@
       (assoc :hours_worked_display (minutes->hours (:minutes_worked p)))
       (assoc :hourly_rate_display (cents->dollars (:hourly_rate_cents p)))))
 
-;; ------------------------------------------------------------
-;; shared list render
-;; ------------------------------------------------------------
+(defn project-id-or-nil [id]
+  (parse-long-safe id))
 
-(defn render-project-list [request template projects]
-  (layout/render request template
-                 {:projects projects
-                  :valid_lists valid-lists
-                  :valid_states valid-states
-                  :valid_priorities valid-priorities
-                  :flash (:flash request)}))
+(defn project-patch [params]
+  {:title (get params "title")
+   :description (get params "description")
+   :details (get params "details")
+   :programming_lang (get params "programming_lang")
+   :is_personal (checkbox-checked? params "is_personal")
+   :has_test_suite (checkbox-checked? params "has_test_suite")
+   :is_open_source (checkbox-checked? params "is_open_source")
+   :list (get params "list")
+   :state (get params "state")
+   :priority (get params "priority")
+   :hourly_rate_cents (some-> (get params "hourly_rate") parse-long-safe (* 100))
+   :minutes_worked (some-> (get params "hours_worked") parse-long-safe (* 60))})
 
-;; ------------------------------------------------------------
-;; list views
-;; ------------------------------------------------------------
+(defn validate-patch [patch]
+  (cond-> {}
+    (and (:list patch) (not (contains? valid-lists (:list patch))))
+    (assoc :list "Invalid list specified")
+
+    (and (:state patch) (not (contains? valid-states (:state patch))))
+    (assoc :state "Invalid state specified")
+
+    (and (:priority patch) (not (contains? valid-priorities (:priority patch))))
+    (assoc :priority "Invalid priority specified")))
 
 (defn list-projects
   [{:keys [query-fn]} request]
@@ -102,14 +110,10 @@
                       (map enrich-project))]
     (render-project-list request "archives.html" projects)))
 
-;; ------------------------------------------------------------
-;; single project
-;; ------------------------------------------------------------
-
 (defn show
   [{:keys [query-fn]}
    {{:keys [id]} :path-params :as request}]
-  (if-let [project-id (parse-long-safe id)]
+  (if-let [project-id (project-id-or-nil id)]
     (if-let [project (query-fn :get-project {:id project-id})]
       (layout/render request "project/show.html"
                      {:project (enrich-project project)
@@ -124,7 +128,7 @@
 (defn edit
   [{:keys [query-fn]}
    {{:keys [id]} :path-params :as request}]
-  (if-let [project-id (parse-long-safe id)]
+  (if-let [project-id (project-id-or-nil id)]
     (if-let [project (query-fn :get-project {:id project-id})]
       (layout/render request "project/edit.html"
                      {:project (enrich-project project)
@@ -136,10 +140,6 @@
                    {:error "Invalid project ID"
                     :flash (:flash request)})))
 
-;; ------------------------------------------------------------
-;; create
-;; ------------------------------------------------------------
-
 (defn create!
   [{:keys [query-fn]}
    {{:strs [project_title
@@ -149,14 +149,10 @@
             is_open_source
             hourly_rate]}
     :form-params
-    :as _request}]
-  (log/debug "FORM:"
-             project_title
-             project_description
-             tech_stack
-             has_test_suite
-             is_open_source
-             hourly_rate)
+    :as request}]
+  (log/debug "create! request:"
+             {:path-params (:path-params request)
+              :form-params (:form-params request)})
   (let [errors (cond-> {}
                  (blank? project_title)
                  (assoc :project_title "Project title is required")
@@ -164,17 +160,13 @@
                  (blank? project_description)
                  (assoc :project_description "Description is required"))
 
-        test-suite? (some? has_test_suite)
-        open-source? (some? is_open_source)
-        hourly-rate (parse-long-safe hourly_rate)
-        hourly-rate-cents (some-> hourly-rate (* 100))
         project-data {:title project_title
                       :description project_description
                       :is_personal false
                       :programming_lang (or tech_stack "clojure")
-                      :has_test_suite test-suite?
-                      :is_open_source open-source?
-                      :hourly_rate_cents hourly-rate-cents}]
+                      :has_test_suite (some? has_test_suite)
+                      :is_open_source (some? is_open_source)
+                      :hourly_rate_cents (some-> hourly_rate parse-long-safe (* 100))}]
     (if (seq errors)
       (-> (response/redirect "/booking")
           (assoc :flash {:errors errors}))
@@ -187,56 +179,26 @@
           (-> (response/redirect "/booking")
               (assoc :flash {:errors {:unknown (.getMessage e)}})))))))
 
-;; ------------------------------------------------------------
-;; update
-;; ------------------------------------------------------------
-
 (defn update!
   [{:keys [query-fn]}
    {{:keys [id]} :path-params
     :as request}]
   (let [params (:form-params request)
-        project-id (parse-long-safe id)
-        list (get params "list")
-        state (get params "state")
-        priority (get params "priority")
-        hourly-rate (parse-long-safe (get params "hourly_rate"))
-        hours-worked (parse-long-safe (get params "hours_worked"))
-        errors (cond-> {}
-                 (and list (not (contains? valid-lists list)))
-                 (assoc :list "Invalid list specified")
-
-                 (and state (not (contains? valid-states state)))
-                 (assoc :state "Invalid state specified")
-
-                 (and priority (not (contains? valid-priorities priority)))
-                 (assoc :priority "Invalid priority specified"))
-
-        patch (present-keys
-               {:title (get params "title")
-                :description (get params "description")
-                :details (get params "details")
-                :is_personal (get params "is_personal")
-                :programming_lang (get params "programming_lang")
-                :has_test_suite (get params "has_test_suite")
-                :is_open_source (get params "is_open_source")
-                :list list
-                :state state
-                :priority priority
-                :hourly_rate_cents (some-> hourly-rate (* 100))
-                :minutes_worked hours-worked})]
+        project-id (project-id-or-nil id)
+        patch (project-patch params)
+        errors (validate-patch patch)]
     (cond
       (nil? project-id)
       (-> (response/redirect "/inbox")
           (assoc :flash {:error "Invalid project ID"}))
 
       (seq errors)
-      (-> (response/redirect (str "/" (or list "inbox")))
+      (-> (response/redirect (str "/" (or (:list patch) "inbox")))
           (assoc :flash {:errors errors}))
 
       :else
       (do
         (log/info "Updating project" project-id "with patch" patch)
         (query-fn :update-project! (assoc patch :id project-id))
-        (-> (http-response/found (str "/" (or list "inbox")))
+        (-> (http-response/found (str "/" (or (:list patch) "inbox")))
             (assoc :flash {:message "Project updated successfully."}))))))
